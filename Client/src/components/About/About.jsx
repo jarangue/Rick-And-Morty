b0b4df1173ee9c7b43f734dfd8ac792d5e956029
Javier Arangue <javier.arangue@gmail.com>
2023-11-03 19:57:12 -0300

    hw

--- Client/src/components/About/About.jsx
+++ Client/src/components/About/About.jsx
@@ -4,7 +4,9 @@ const About = () => {
 
     return(
         <h2 className={style.text}>
-            Este es el Componente About
+             About
+
+            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quis adipisci molestiae eos cumque animi quae, tempora ullam dolore nulla voluptatibus ipsum quisquam cupiditate distinctio maiores aperiam qui totam? Tempora.</h3>
 
         </h2>
     )
